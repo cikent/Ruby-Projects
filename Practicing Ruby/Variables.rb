94538003795cfb5b6cc0f826a5e1d676aed7be25
Chris Kent <christopherikent@gmail.com>
2020-01-02 11:26:44 -0800

    Updating Ruby-Projects repo

---
+++ Practicing Ruby/Variables.rb
@@ -0,0 +1,18 @@
+# VARIABLES
+=begin
+Names are care-sensitive and may begin with:
+    letters, _
+After, may include:
+    letters, numbers, _
+Convention says
+    Start with a lowercase word, then additional words are lowercase seperated by an underscore
+    example: my_example_variable
+=end
+
+name = "Chris"      # Strings
+age = "31"          # Integer
+pie = 3.14159       # Decimal | Float | Double
+is_parent = true    # Boolean -> True | False
+
+puts "My name is #{name}"
+puts "My name is " + name
