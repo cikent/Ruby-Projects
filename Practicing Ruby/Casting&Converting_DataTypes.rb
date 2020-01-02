b91f9f742fdad1a78024d82d3178a64080dc0de1
Chris Kent <christopherikent@gmail.com>
2020-01-02 15:58:20 -0800

    Updating Ruby Projects repo to remove old folders

--- Practicing Ruby/Casting&Converting_DataTypes.rb
+++
@@ -1,8 +0,0 @@
-# CASTING & CONVERTING DATA TYPES
-
-puts 3.14.to_i          # To Integer
-puts 3.to_f             # To Float
-puts 3.0.to_s           # To String
-
-puts 100 + "50".to_i    # Takes the Integer and add's the String is converted to an Integer
-puts 100 + "50.99".to_f # Takes the Integer and add's the String is converted to a Float
--- Practicing Ruby/Printing.rb
+++
@@ -1,4 +0,0 @@
-# PRINTING
-puts "Hello" # put adds a newline after the String
-print "World" # print does not add a new line after the String 
-puts "!"
